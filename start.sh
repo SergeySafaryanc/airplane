ad0d60dc73de9a3d3bcf854c11a55cff1bcb1822
Sergey <36757338+SergeySafaryanc@users.noreply.github.com>
2019-12-23 18:14:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 LOG4J2CONF="conf/log4j2.properties"
 CONFIG="conf/config.properties"
 
-java -Dlog4j2.configurationFile=file:$LOG4J2CONF -Dconfig=$PWD/$CONFIG -jar test.jar
+java -Dlog4j2.configurationFile=file:$LOG4J2CONF -Dconfig=$PWD/$CONFIG -jar aiplane.jar
