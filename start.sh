1253666f63f3370d45199ea35bbb82958bd9252f
Sergey <safaryancs@yahoo.com>
2020-01-09 20:04:50 +0300

    .

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 LOG4J2CONF="conf/log4j2.properties"
 CONFIG="conf/config.properties"
 
-java -Dlog4j2.configurationFile=file:$LOG4J2CONF -Dconfig=$PWD/$CONFIG -jar airplane.jar
+java -Dlog4j2.configurationFile=$LOG4J2CONF -Dconfig=$CONFIG -jar airplane.jar
